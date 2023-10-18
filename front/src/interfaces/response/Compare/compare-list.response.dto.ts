c73765b1b889128954a3da58f8813228391aa43f
SeowooNote <dkr0210@naver.com>
2023-10-18 12:43:31 +0900

    1018

---
+++ front/src/interfaces/response/Compare/compare-list.response.dto.ts
@@ -0,0 +1,8 @@
+export default interface CompareListResponseDto {
+    compareNumber : number;
+    compareControlGroup : string;
+    compareExperimentalGroup : string;
+    compareDatetime : string;
+    compareResult : string;
+    compareUserEmail: string;
+}
